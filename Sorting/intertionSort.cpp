5eb9f97b505f56454014c4f9ed2a6a9ee9458820
gorvsngh <gk914670@gmail.com>
2025-07-30 22:58:53 +0530

    insertion sort

---
+++ Sorting/intertionSort.cpp
@@ -0,0 +1,25 @@
+#include <iostream>  
+using namespace std;
+
+int main() {
+    int arr[] = {5, 4, 6, 3, 2}; 
+    int n = sizeof(arr) / sizeof(arr[0]);
+    
+    for (int i = 0; i < n - 1; i++) {
+        int minIndex = i;
+
+        for (int j = i + 1; j < n; j++) {
+            if (arr[j] < arr[minIndex]) {
+                minIndex = j;
+            }
+        }
+        swap(arr[i], arr[minIndex]);
+    }
+
+    cout << "Sorted array: ";
+    for (int i = 0; i < n; i++)
+        cout << arr[i] << " ";
+    cout << endl;
+/ 
+    return 0;
+}
