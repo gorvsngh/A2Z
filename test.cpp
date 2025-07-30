4bf165601070c910abdcbf74459d1ba8a10e5074
gorvsngh <gk914670@gmail.com>
2025-07-30 21:57:48 +0530

    first commit

---
+++ test.cpp
@@ -0,0 +1,8 @@
+#include <iostream>     // Preprocessor directive
+
+using namespace std;    // So we don't need to write std:: before cout
+
+int main() {            // Main function - execution starts here
+    cout << "Hello, World!";   // Output statement
+    return 0;           // Ends the program
+}
