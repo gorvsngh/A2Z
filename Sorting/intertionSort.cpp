aac8cc6e593d61c103c9da05f375ebc82b724d86
gorvsngh <gk914670@gmail.com>
2025-08-01 17:09:36 +0530

    insertion sort

--- Sorting/intertionSort.cpp
+++ Sorting/intertionSort.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 
 int main() {
-    int arr[] = {5, 4, 6, 3, 2}; 
+    int arr[] = {5, 4, 6, 3}; 
     int n = sizeof(arr) / sizeof(arr[0]);
     
     for (int i = 0; i < n - 1; i++) {
@@ -20,6 +20,6 @@ int main() {
     for (int i = 0; i < n; i++)
         cout << arr[i] << " ";
     cout << endl;
-/ 
+ 
     return 0;
 }
